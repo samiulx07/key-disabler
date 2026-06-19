3ddf703a8ae036ca8e450ac5b0efb1d00d4fc5d8
Samiul Hasan <71167851+SamiulxHasanx07@users.noreply.github.com>
2026-06-20 00:50:40 +0600

    Add tray icon service

---
+++ src/KeyDisabler.App/Services/TrayIconService.cs
@@ -0,0 +1,65 @@
+using System.Drawing;
+using System.Windows;
+using Forms = System.Windows.Forms;
+
+namespace KeyDisabler.App.Services;
+
+public sealed class TrayIconService : IDisposable
+{
+    private readonly Window _window;
+    private readonly Forms.NotifyIcon _notifyIcon;
+    private bool _isDisposed;
+
+    public TrayIconService(Window window)
+    {
+        _window = window;
+
+        var menu = new Forms.ContextMenuStrip();
+        menu.Items.Add("Open", null, (_, _) => ShowWindow());
+        menu.Items.Add("Hide", null, (_, _) => _window.Hide());
+        menu.Items.Add(new Forms.ToolStripSeparator());
+        menu.Items.Add("Exit", null, (_, _) => ExitApplication());
+
+        _notifyIcon = new Forms.NotifyIcon
+        {
+            Text = "Key Disabler",
+            Icon = SystemIcons.Application,
+            Visible = true,
+            ContextMenuStrip = menu
+        };
+
+        _notifyIcon.DoubleClick += (_, _) => ShowWindow();
+    }
+
+    public void ShowBalloon(string title, string message)
+    {
+        _notifyIcon.BalloonTipTitle = title;
+        _notifyIcon.BalloonTipText = message;
+        _notifyIcon.ShowBalloonTip(2500);
+    }
+
+    private void ShowWindow()
+    {
+        _window.Show();
+        _window.WindowState = WindowState.Normal;
+        _window.Activate();
+    }
+
+    private void ExitApplication()
+    {
+        Dispose();
+        System.Windows.Application.Current.Shutdown();
+    }
+
+    public void Dispose()
+    {
+        if (_isDisposed)
+        {
+            return;
+        }
+
+        _notifyIcon.Visible = false;
+        _notifyIcon.Dispose();
+        _isDisposed = true;
+    }
+}
